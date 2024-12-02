18bf9d827564d1a8eb5efd2424193ce6bbb62c27
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-12-02 14:09:22 +0300

    final fix

---
+++ src/Data.js
@@ -0,0 +1,48 @@
+const Data = [
+    {
+        name: " Jinx",
+        image: "./src/assets/jinx.png",
+        description: "A chaotic and unpredictable character known for her love of destruction and mayhem." +
+            "Jinx, originally known as Powder, is a vibrant and chaotic character from the undercity of Zaun. With her wild, electric blue hair and mismatched clothing, she embodies a sense of unpredictability and rebellion. Jinx is a master of mayhem, thriving on chaos and destruction, which she often orchestrates with a playful, almost childlike glee.\n" +
+            "\n" +
+            "Once a sweet and innocent girl, Jinx's transformation into a notorious criminal mastermind is marked by her tumultuous past and the trauma of losing her family. After a series of tragic events, including the death of her adoptive father Vander and her estrangement from her sister Vi, Jinx adopts a new identity that embraces her darker impulses.\n" +
+            "\n" +
+            "Jinx is known for her signature weapons, including her explosive \"Fishbones\" rocket launcher and her \"Doodle\" grenades, which she uses to create chaos and instill fear in her enemies. Her quirky personality is characterized by her love for pranks, her obsession with destruction, and her tendency to speak in a rapid, erratic manner, reflecting her unstable mental state.\n" +
+            "\n" +
+            "Beneath her manic exterior, Jinx grapples with feelings of abandonment, guilt, and a desperate longing for her sister's approval. Her relationship with Vi is complex, filled with love, resentment, and a desire for reconciliation, making her character arc one of the most compelling in the series.\n" +
+            "\n" +
+            "Jinx's journey in \"Arcane\" explores themes of identity, family, and the impact of trauma, showcasing her evolution from a fragile girl into a formidable force of chaos. Despite her villainous tendencies, there remains a glimmer of the girl she once was, making her a tragic yet captivating character in the world of Piltover and Zaun."
+    },
+    {
+        name: "Vi",
+        image: "./src/assets/Vi.png",
+        description: "A tough and determined enforcer with a strong sense of justice, known for her powerful punches." +
+            "Vi, a fierce and determined character from the undercity of Zaun, is known for her hotheaded nature and exceptional combat skills. With her signature pink hair and powerful gauntlets, she embodies strength and resilience. Vi's journey is marked by her transformation from a street kid into a vigilante, driven by a desire to protect her loved ones and seek justice in a world rife with corruption.\n" +
+            "\n" +
+            "Growing up in a harsh environment, Vi developed a tough exterior and a strong sense of loyalty, particularly towards her sister, Jinx. Their complicated relationship is central to her character arc, as Vi grapples with feelings of guilt and responsibility for Jinx's choices. Despite her tough demeanor, Vi has a deep emotional core, often struggling with the weight of her past and the choices she must make to forge a better future.\n" +
+            "\n" +
+            "Vi's combat style is characterized by her agility and brute strength, utilizing her gauntlets to deliver powerful punches and engage in close-quarters combat. She is not only a skilled fighter but also a strategic thinker, often using her instincts to navigate the dangers of her environment.\n" +
+            "\n" +
+            "Throughout \"Arcane,\" Vi's character explores themes of family, redemption, and the moral complexities of justice. Her journey is one of self-discovery, as she learns to balance her desire for vengeance with the need for compassion and understanding."
+    },
+    {
+        name: "Caitlyn",
+        image: "./src/assets/caitlin.png",
+        description: "The sheriff of Piltover, known for her sharp wit and exceptional marksmanship." +
+            "Caitlyn Kiramman is a skilled enforcer from Piltover, known for her intelligence, determination, and strong sense of justice. Raised in a wealthy family, she initially appears sheltered but is deeply compassionate and committed to uncovering the truth behind the criminal activities plaguing her city. Caitlyn's journey is marked by her pursuit of justice, her complex relationships, and her moral dilemmas as she navigates the challenges of her role as an enforcer."
+    },
+    {
+        name: "Jayce",
+        image: "./src/assets/jayce.png",
+        description: "A brilliant inventor and defender of Piltover, wielding a powerful hammer." +
+            "Jayce Talis is a brilliant inventor and a key figure in the political landscape of Piltover. He is known for his exceptional intellect, innovative spirit, and strong desire to advance technology for the betterment of society. As a member of the prestigious council, Jayce grapples with the responsibilities of leadership while navigating the complexities of his ambitions and the moral implications of his inventions"
+    },
+    {
+        name: "Silco",
+        image: "./src/assets/silco.png",
+        description: "A cunning and ambitious character with a complex relationship with power and control." +
+            "Silco is a prominent character in the animated series \"Arcane,\" serving as a major supporting character and the adoptive father of Jinx. He is the ambitious leader of the Zaunite chem-barons, manipulating the undercity's dynamics while harboring a deep-seated grudge against his former ally, Vander.\n" +
+            "\n"
+    }
+];
+export default Data
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom";
+import Header from "./components/Header.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <StrictMode>
+            <Header />
+            <App />
+        </StrictMode>
+    </BrowserRouter>
+  ,
 )
